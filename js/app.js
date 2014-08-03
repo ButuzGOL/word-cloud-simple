d067fef05e0a9957ee08ddef2080b28be9bb36ff
ButuzGOL <ButuzGOL@gmail.com>
2014-08-03 21:50:20 +0300

    Cloud of topics

--- js/app.js
+++ js/app.js
@@ -20,6 +20,63 @@ App.TopicsRoute = Ember.Route.extend({
   }
 });
 
+App.TopicController = Ember.ObjectController.extend({
+  volumes: {
+    max: null,
+    min: null
+  },
+  popularity: function() {
+    var model,
+        min, max, middle,
+        minPoint = 1, maxPoint = 6,
+        volume = this.get('volume'),
+        popularity;
+
+    if (!this.volumes.max || !this.volumes.min) {
+      model = this.get('parentController.model');
+
+      max = 0;
+      min = model[0].volume;
+      model.forEach(function(item) {
+        var volume = item.volume;
+
+        if (volume > max) {
+          max = volume;
+        }
+
+        if (volume < min) {
+          min = volume;
+        }
+      });
+      this.volumes.max = max;
+      this.volumes.min = min;
+    } else {
+      max = this.volumes.max;
+      min = this.volumes.min;
+    }
+
+    middle = max - min;
+
+    popularity = ((volume - min) / middle) * (maxPoint - minPoint) + minPoint;
+
+    return 'font-size: ' + (popularity * 3 + 14) + 'px;';
+  }.property('volume'),
+  mood: function() {
+    var sentimentScore = this.get('sentimentScore'),
+        sentimentClassName;
+
+    if (sentimentScore > 60) {
+      sentimentClassName = 'green';
+    } else if (sentimentScore < 40) {
+      sentimentClassName = 'red';
+    } else {
+      sentimentClassName = 'grey';
+    }
+
+    return sentimentClassName;
+  }.property('sentimentScore')
+});
+
 App.TopicRoute = Ember.Route.extend({
   model: function(params) {
     return Topics.findBy('id', params.topic_id);
